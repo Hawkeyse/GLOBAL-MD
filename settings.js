c965281b051917eaa2a390cc9b643e40dc266de6
Timtech <96210116+Hawkeyse@users.noreply.github.com>
2024-08-31 12:42:30 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,24 +4,24 @@ const fs = require('fs');
 const chalk = require('chalk');
 
 //owmner v card
-global.ytname = "YT: GlobalTechInfo" //ur yt chanel name
-global.socialm = "GitHub: GlobalTechInfo" //ur github or insta name
-global.location = "Pakistan, Karachi, Karachi" //ur location
+global.ytname = "YT: Timtech" //ur yt chanel name
+global.socialm = "GitHub: Timtech" //ur github or insta name
+global.location = "Nigeria, Ghana" //ur location
 
 //new
 global.botname = '© 𝐆𝐋𝐎𝐁𝐀𝐋-𝐌𝐃' //ur bot name
-global.ownernumber = ['923444844060'] //ur owner number, dont add more than one
-global.ownername = '© GlobalTechInfo' //ur owner name
-global.websitex = "https://www.youtube.com/@GlobalTechInfo"
-global.wagc = "https://whatsapp.com/channel/0029VagJIAr3bbVBCpEkAM07"
+global.ownernumber = ['233595885532'] //ur owner number, dont add more than one
+global.ownername = '© timtech' //ur owner name
+global.websitex = ""
+global.wagc = ""
 global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
-global.botscript = 'https://github.com/GlobalTechInfo/GLOBAL-MD' //script link
-global.packname = "Sticker By"
-global.author = "GlobalTechInfo\n\n+923444844060"
-global.creator = "923444844060@s.whatsapp.net"
+global.wm = "TribalGamerNetwork."
+global.botscript = '' //script link
+global.packname = "Sticker By TribalGamerNetwork"
+global.author = "TribalGamerNetwork"
+global.creator = "233595885532"
 global.xprefix = '.'
-global.premium = ["923444844060"] // Premium User
+global.premium = ["233595885532"] // Premium User
 
 //channel id
 global.xchannel = {
